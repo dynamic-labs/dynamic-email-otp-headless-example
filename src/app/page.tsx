7b1b01205be9bba0c08f2ea81233c25b869e8527
Andreas Wiermann Casas <andreas.w.dev@gmail.com>
2024-03-26 15:37:21 -0300

    add dynamic and authenticate with email OTP headless

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,80 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+
+import {
+  useConnectWithEmailOtp,
+  useDynamicContext,
+} from "@dynamic-labs/sdk-react-core";
+import { FormEventHandler, useState } from "react";
 
 export default function Home() {
+  const { user, handleLogOut } = useDynamicContext();
+  const [showOtpForm, setShowOtpForm] = useState(false);
+
+  const { connectWithEmail, verifyOneTimePassword, retryOneTimePassword } =
+    useConnectWithEmailOtp();
+
+  const showEmailForm = !user && !showOtpForm;
+  const showOtp = !user && showOtpForm;
+
+  if (showEmailForm) {
+    const handleSendOtp: FormEventHandler<HTMLFormElement> = async (event) => {
+      event.preventDefault();
+      event.stopPropagation();
+
+      const formData = new FormData(event.currentTarget);
+
+      const email = formData.get("email") as string;
+
+      await connectWithEmail(email);
+      setShowOtpForm(true);
+    };
+
+    return (
+      <form key="email-form" onSubmit={handleSendOtp}>
+        <label>
+          Email
+          <input type="email" name="email" />
+        </label>
+        <br />
+        <button type="submit">Send OTP</button>
+      </form>
+    );
+  }
+
+  if (showOtp) {
+    const handleRetryOtp: FormEventHandler<HTMLFormElement> = async (event) => {
+      event.preventDefault();
+      event.stopPropagation();
+
+      const formData = new FormData(event.currentTarget);
+
+      const oneTimePassword = formData.get("oneTimePassword") as string;
+
+      await verifyOneTimePassword(oneTimePassword);
+      setShowOtpForm(false);
+    };
+
+    return (
+      <form key="otp-form" onSubmit={handleRetryOtp}>
+        <label>
+          OTP
+          <input type="text" name="oneTimePassword" />
+        </label>
+        <br />
+        <button type="submit">Verify OTP</button>
+        <br />
+        <button type="button" onClick={retryOneTimePassword}>
+          Re-send OTP
+        </button>
+      </form>
+    );
+  }
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <main>
+      <h1>Welcome {user?.email}</h1>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <button onClick={handleLogOut}>Logout</button>
     </main>
   );
 }
